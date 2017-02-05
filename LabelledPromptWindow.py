349a46b78719d6a9ef49f57f355778246d5bcae3
Carl Findahl <sportleroro@gmail.com>
2017-02-04 20:15:51 -0800

    Cleaned up testing code for LabelledPromptWindow

--- LabelledPromptWindow.py
+++ LabelledPromptWindow.py
@@ -1,9 +1,9 @@
-import sys
 from PyQt5 import QtWidgets
 
-class LabelledPromptWindow(QtWidgets.QWidget):
+class LabelledPromptWindow(QtWidgets.QDialog):
     def __init__(self, Prompt: str, ButtonText="Confirm"):
-        """A basic Horizontally layed out Prompt Window.
+        """A basic Horizontally layed out Prompt Window
+        with a QLineEdit for accepting user input.
 
         Arguments:
         string Prompt -- The prompt to provide the user
@@ -27,7 +27,7 @@ class LabelledPromptWindow(QtWidgets.QWidget):
 
         self._confirmButton = QtWidgets.QPushButton(self)
         self._confirmButton.setText(ButtonText)
-        self._confirmButton.clicked.connect(self.provideUserInput)
+        self._confirmButton.clicked.connect(self.confirmClicked)
 
         # PRIVATE
         __layout = QtWidgets.QVBoxLayout(self)
@@ -38,17 +38,10 @@ class LabelledPromptWindow(QtWidgets.QWidget):
 
         self.setLayout(__layout)
 
-    def provideUserInput(self):
-        """Closes the widget and returns the user input"""
-        self.close()
-        return self.getInputText()
+    def confirmClicked(self):
+        """Will close the window"""
+        self.accept()
 
     def getInputText(self):
         """Return the string value currently in self._inputField"""
         return self._inputField.text()
-
-if __name__ == '__main__':
-    Application = QtWidgets.QApplication(sys.argv)
-    FilePathInput = LabelledPromptWindow("Enter a filepath:", "Confirm")
-    print(FilePathInput.show())
-    sys.exit(Application.exec_())
